38bcd465b1cef705413e4abb52d63a76b15ebf5e
Ross Fenning <ross.fenning@gmail.com>
2014-07-27 17:36:12 +0100

    Added some more tests to enable following links.

--- hyperspace.py
+++ hyperspace.py
@@ -1,13 +1,42 @@
+import collections
 from rdflib import Graph
+import requests
+from bs4 import BeautifulSoup
+import urlparse
+
+
+class Link(object):
+    def __init__(self, href):
+        self.href = href
+
+    def follow(self):
+        return jump(self.href)
 
 
 class Page(object):
-    def __init__(self, data=None):
-        self.data = data
+    def __init__(self, response):
+        self.url = response.url
 
-def parse(html_string):
-    g = Graph()
-    g.parse(data=html_string, format='html')
+        self.data = Graph()
+        self.data.parse(data=response.text, format='html')
 
-    page = Page(data=g)
-    return page
+        soup = BeautifulSoup(response.text)
+
+        # We use a dictionary to allow lookup of links by rel
+        self.links = collections.defaultdict(list)
+        # Find all <a/> tags
+        for a_tag in soup.find_all('a'):
+            # For now, we only consider <a/> tags with rel attributes
+            if 'rel' in a_tag.attrs:
+                # The rel attribute can be multivalued, so it's probably
+                # alright to duplicate a link instance against multiple rel keys
+                for rel in a_tag['rel']:
+                    # Allow for href values to be relative...
+                    absolute_href = urlparse.urljoin(self.url, a_tag['href'])
+                    link = Link(absolute_href)
+                    self.links[rel].append(link)
+
+
+
+def jump(url):
+    return Page(requests.get(url))
--- test_hyperspace.py
+++ test_hyperspace.py
@@ -1,18 +1,58 @@
 import unittest
 from rdflib import URIRef, Literal
-from hyperspace import parse
-import os
+import hyperspace
+import responses
 
 
 class MicrodataTest(unittest.TestCase):
+
+    def setUp(self):
+        responses.start()
+
+        with open('./fixtures/users.html', 'rb') as fixture:
+            responses.add(responses.GET, 'http://example.com/users/',
+                          body=fixture.read(), status=200,
+                          content_type='text/html')
+
+        with open('./fixtures/fiona.html', 'rb') as fixture:
+            responses.add(responses.GET, 'http://example.com/users/fiona',
+                          body=fixture.read(), status=200,
+                          content_type='text/html')
+
+    def tearDown(self):
+        responses.stop()
+        responses.reset()
+
     def test_finds_itemscope_with_basic_attributes(self):
-        print os.getcwd()
-        with open('./fixtures/microdata.html', 'rb') as fixture:
-            page = parse(fixture.read())
-
-        fact = (URIRef('http://example.com/fiona'), URIRef('http://schema.org/name'), Literal('Fiona Bennett'))
-
+        # Given
+        fact = (URIRef('http://example.com/users/fiona#id'),
+                URIRef('http://schema.org/name'),
+                Literal('Fiona Bennett'))
+        # When
+        page = hyperspace.jump('http://example.com/users/fiona')
+        # Then
         self.assertIn(fact, page.data)
+
+    def test_finds_link_in_page(self):
+        # When - We visit the users list page
+        page = hyperspace.jump('http://example.com/users/')
+        # Then - We should find there are some "user" links
+        print page.links
+        self.assertIn('user', page.links)
+
+    def test_follows_links(self):
+        # Given - Information we know should be held about a user
+        fact = (URIRef('http://example.com/users/fiona#id'),
+                URIRef('http://schema.org/name'),
+                Literal('Fiona Bennett'))
+        # When - We visit the users list page
+        page = hyperspace.jump('http://example.com/users/')
+        # And - Follow the link to the user in question
+        page = page.links['user'][0].follow()
+        # Then - We should find a known fact about that user
+        self.assertIn(fact, page.data)
+
+
 
 if __name__ == '__main__':
     unittest.main()
