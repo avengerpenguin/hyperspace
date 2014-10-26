923c6f1fc30f2f58310fe3c831977217812887a2
Ross Fenning <fennir01@mc-s083545.local>
2014-10-26 14:29:40 +0000

    Added Accept headers and ensured response comes back after a POST.

--- hyperspace/__init__.py
+++ hyperspace/__init__.py
@@ -6,18 +6,26 @@ import cgi
 
 
 session = requests.Session()
+session.headers['User-Agent'] = 'Hyperspace (https://github.com/avengerpenguin/hyperspace)'
+session.headers['Accept'] = 'text/html,application/ld+json,text/turtle'
+
+
+def response_to_page(response):
+    mime = cgi.parse_header(response.headers['Content-Type'])
+    return mime_to_page(mime[0], **mime[1])(response)
 
 
 def jump(url):
     response = session.get(url)
-    mime = cgi.parse_header(response.headers['Content-Type'])
-    return mime_to_page(mime[0], **mime[1])(response)
+    return response_to_page(response)
 
 
 def send(url, data, _):
     response = session.post(url, data=data)
     if 'Location' in response.headers:
         return jump(response.headers['Location'])
+    else:
+        return response_to_page(response)
 
 
 def mime_to_page(mime, **kwargs):
